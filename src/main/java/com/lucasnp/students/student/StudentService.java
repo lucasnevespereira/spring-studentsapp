4b68a16f5bb9c7fa4707002d7a6c12b3301c14fa
lucasnevespereira <pereiraneveslucas@gmail.com>
2021-04-08 16:09:16 +0200

    implemented update student

--- src/main/java/com/lucasnp/students/student/StudentService.java
+++ src/main/java/com/lucasnp/students/student/StudentService.java
@@ -3,7 +3,9 @@ package com.lucasnp.students.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 // We use @Service or @Component to say that student service is a
@@ -40,5 +42,26 @@ public class StudentService {
         }
 
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional // @Transactional avoids us to have to do sql queries by setting the entity(student) to a temporary state
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException("Student with id " + studentId + "does not exists"));
+
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("email already exists");
+            }
+
+            student.setEmail(email);
+        }
     }
 }
