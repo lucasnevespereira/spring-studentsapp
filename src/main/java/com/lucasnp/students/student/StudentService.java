d0d5f02db03e0aec5d6a0277e60b1e16da096a30
lucasnevespereira <pereiraneveslucas@gmail.com>
2021-04-08 15:25:44 +0200

    implemented POST route

--- src/main/java/com/lucasnp/students/student/StudentService.java
+++ src/main/java/com/lucasnp/students/student/StudentService.java
@@ -20,4 +20,8 @@ public class StudentService {
     public List<Student> getAllStudents() {
         return studentRepository.findAll();
     }
+
+    public void addNewStudent(Student student) {
+        System.out.println(student);
+    }
 }
