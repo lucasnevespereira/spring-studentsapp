d8cd5955c4345cf4b53de05d763fbece2b9f8dfa
lucasnevespereira <pereiraneveslucas@gmail.com>
2021-04-08 12:16:26 +0200

    returning hello world as a json array

--- src/main/java/com/lucasnp/students/StudentsApplication.java
+++ src/main/java/com/lucasnp/students/StudentsApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class StudentsApplication {
@@ -14,7 +16,7 @@ public class StudentsApplication {
     }
 
     @GetMapping
-    public String hello() {
-        return "Hello World";
+    public List<String> hello() {
+        return List.of("Hello", "World");
     }
 }
